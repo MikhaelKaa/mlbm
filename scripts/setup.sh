d87560cf368cff70334fbed304efea852ec3af1d
Mikhail.Kargapolcev <KargapolcevME@sinara-group.com>
2024-08-31 01:15:17 +0300

    fix: Удалил лишнее

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -52,17 +52,3 @@ else
    echo "zx0 packer not build"
    cd ./tools/zx0/; make; cd ../..
 fi
-
-#mkdir ./bsp -p || true
-#mkdir ./layers -p || true
-
-# Есть в репозитории
-#git -C ./bsp clone -b dunfell  https://github.com/vasilievsv/meta-radxa || true
-#git -C ./bsp clone -b dunfell  https://github.com/radxa/meta-radxa.git || true
-
-#git -C ./layers clone -b dunfell https://git.yoctoproject.org/poky.git || true
-#git -C ./layers clone -b dunfell https://git.openembedded.org/meta-openembedded || true
-#git -C ./layers clone -b dunfell https://github.com/YoeDistro/meta-python2.git || true
-#git -C ./layers clone -b dunfell https://git.yoctoproject.org/meta-security.git || true
-#git -C ./layers clone -b dunfell https://git.yoctoproject.org/meta-virtualization  || true
-#git -C ./layers clone -b dunfell https://github.com/DynamicDevices/meta-mono.git
